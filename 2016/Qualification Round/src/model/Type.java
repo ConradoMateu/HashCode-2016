4811e08ab62e64800c2dcf815f424ae797f82410
Antonio López Marín <tonilopezmr@gmail.com>
2016-02-11 21:21:55 +0100

    first version of model

---
+++ 2016/Qualification Round/src/model/Type.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Created by toni on 11/02/16.
+ */
+public class Type {
+
+    private int type;
+    private int weight;
+
+    public Type(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
